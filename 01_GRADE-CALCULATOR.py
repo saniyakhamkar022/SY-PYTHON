24c538a94475b36370ab39be660fd20f7ea7aaa6
saniyakhamkar022 <saniyakhamkar22@gmail.com>
2025-06-23 09:47:40 +0530

    Add files via upload

---
+++ 01_GRADE-CALCULATOR.py
@@ -0,0 +1,16 @@
+
+score = int(input("Enter the student's score: "))
+
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+
+print(f"The student's grade is: {grade}")
+
